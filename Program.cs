b19021e03df023f799d0df2fa5dd1cf2514791ab
Jeshua Herrera Bedoya <Jeshuah71>
2023-10-03 19:01:31 -0600

    practicefinished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(Planets.Pluto + "is planet"); 
+Console.WriteLine(Planets.Mercury + "is planet #" +(int)Planets.Mercury);
+Console.WriteLine(Planets.Pluto + "is a planet #" + (int)Planets.Pluto);
+string name = PlanetRadius.Earth.ToString();
+int radius = (int)PlanetRadius.Earth;
+double volume = Volume(PlanetRadius.Earth);
+Console.WriteLine("Planet" + name);
+Console.WriteLine("Radius:" + radius + " km");
+Console.WriteLine("Volume: " + volume + " km^3");
+
+double Volume(PlanetRadius radius)
+{
+    double volume = (4.0/ 3.0) * Math.PI * Math.Pow((int) radius , 3);
+    return volume;
+}
+
+
+
+
+enum Planets
+{
+    Mercury = 1,
+    Venus = 2 ,
+    Earth = 3,
+    Mars = 4,
+    Jupiter = 5, 
+    Saturn = 6,
+    Uranus = 7,
+    Neptune = 8,
+    Pluto = 9
+}
+enum PlanetRadius
+{
+    Mercury = 2439,
+    Venus = 6051 ,
+    Earth = 6371,
+    Mars = 3389,
+    Jupiter = 69911, 
+    Saturn = 58232,
+    Uranus = 25362,
+    Neptune = 24622,
+    Pluto = 1188
+}
